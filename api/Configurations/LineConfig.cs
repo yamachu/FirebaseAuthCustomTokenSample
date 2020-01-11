f2d503231e00c9410ec98c8115fa5705569e0e63
yamachu <yamachu.dev@gmail.com>
2020-01-12 03:29:26 +0900

    Add LineConfig and inject settings

---
+++ api/Configurations/LineConfig.cs
@@ -0,0 +1,9 @@
+namespace Example.Function.Configurations
+{
+	public class LineConfig
+	{
+		public string LineClientId { get; set; }
+		public string LineClientSecret { get; set; }
+		public string LineRedirectUrl { get; set; }
+	}
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Linq;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Example.Function.Configurations;
 
 [assembly: FunctionsStartup(typeof(Example.Function.Startup))]
 namespace Example.Function
@@ -7,7 +14,22 @@ namespace Example.Function
 	{
 		public override void Configure(IFunctionsHostBuilder builder)
 		{
-			// Todo: impl
+			var executioncontextoptions = builder.Services.BuildServiceProvider()
+				.GetService<IOptions<ExecutionContextOptions>>().Value;
+			var currentDirectory = executioncontextoptions.AppDirectory;
+
+			var config = new ConfigurationBuilder()
+				.SetBasePath(currentDirectory)
+				.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+				.AddEnvironmentVariables()
+				.Build();
+
+			builder.Services.AddSingleton<IConfiguration>(config);
+
+			builder.Services
+				.AddOptions<LineConfig>()
+				.Configure<IConfiguration>((settings, configuration) => configuration.Bind(settings))
+				.Validate((c) => !new[] { c.LineClientId, c.LineClientSecret, c.LineRedirectUrl }.Any(s => String.IsNullOrWhiteSpace(s)));
 		}
 	}
 }
